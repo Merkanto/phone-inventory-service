7e600bb251833567eb15409fe8578639f498c4fd
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-07-14 22:48:36 +0300

    Init commit.

---
+++ src/test/java/phoneinventoryservice/phoneinventoryservice/PhoneInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package phoneinventoryservice.phoneinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
